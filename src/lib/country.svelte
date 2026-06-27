61ed36e83eb3c53b4062c4964e89dc84d6de5490
Benjamin Wright <benwboston@gmail.com>
2026-06-26 22:05:49 -0400

    Fix dynamic rendering build error

--- src/lib/country.svelte
+++ src/lib/country.svelte
@@ -1,46 +1,48 @@
 <script lang="ts">
-  import { countPixels } from "$lib";
-  import { PieChart } from "layerchart";
+    import { countPixels } from "$lib";
+    import { PieChart } from "layerchart";
 
-  const props: { label: string; code: string } = $props();
+    const props: { label: string; code: string } = $props();
 
-  const map = $derived(await countPixels(`https://flagcdn.com/${props.code}.svg`));
+    const map = $derived(
+        await countPixels(`https://flagcdn.com/${props.code}.svg`),
+    );
 </script>
 
 <div class="p-10 flex flex-col gap-10">
-  <a class="w-full text-white text-3xl font-bold" href="./{props.code}">
-    {props.label}
-  </a>
+    <a class="w-full text-white text-3xl font-bold" href="./?c={props.code}">
+        {props.label}
+    </a>
 
-  <div class="flex items-center justify-center">
-    <figure class="w-1/2 p-2">
-      <img src="https://flagcdn.com/{props.code}.svg" alt={props.label} />
-    </figure>
-    <figure class="w-1/2 p-2">
-      <PieChart
-        outerRadius={160}
-        innerRadius={-20}
-        cornerRadius={10}
-        padAngle={0.02}
-        data={map}
-        key="color"
-        value="percentage"
-        cRange={map.map((e) => e.color)}
-        height={300}
-        class="text-white"
-        labels={{
-          placement: "callout",
-          offset: 1,
-          value: (d: { percentage: number; color: string }) =>
-            d.color === "white"
-              ? "other"
-              : d.percentage.toLocaleString(undefined, {
-                  style: "percent",
-                  maximumFractionDigits: 1,
-                }),
-          class: "text-xs fill-white",
-        }}
-      />
-    </figure>
-  </div>
+    <div class="flex items-center justify-center">
+        <figure class="w-1/2 p-2">
+            <img src="https://flagcdn.com/{props.code}.svg" alt={props.label} />
+        </figure>
+        <figure class="w-1/2 p-2">
+            <PieChart
+                outerRadius={160}
+                innerRadius={-20}
+                cornerRadius={10}
+                padAngle={0.02}
+                data={map}
+                key="color"
+                value="percentage"
+                cRange={map.map((e) => e.color)}
+                height={300}
+                class="text-white"
+                labels={{
+                    placement: "callout",
+                    offset: 1,
+                    value: (d: { percentage: number; color: string }) =>
+                        d.color === "white"
+                            ? "other"
+                            : d.percentage.toLocaleString(undefined, {
+                                  style: "percent",
+                                  maximumFractionDigits: 1,
+                              }),
+                    class: "text-xs fill-white",
+                }}
+            />
+        </figure>
+    </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+  import { page } from "$app/state";
   import { getRandomIntInclusive } from "$lib";
   import countries from "$lib/countries.json";
   import Country from "$lib/country.svelte";
 
-  const randomCountry =
+  const code = page.url.searchParams.get("c");
+  const country =
+    countries.find((c) => c.code === code) ??
     countries[getRandomIntInclusive(0, countries.length - 1)];
 </script>
 
 <svelte:head>
-  <link rel="icon" href="https://flagcdn.com/{randomCountry.code}.svg" />
+  <link rel="icon" href="https://flagcdn.com/{country.code}.svg" />
 </svelte:head>
 
-<Country {...randomCountry} />
+<Country {...country} />
